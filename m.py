1a65d4663725363b2e4a312050e1e4c647c27ea4
grundmeierl <63403753+grundmeierl@users.noreply.github.com>
2020-04-11 02:23:24 -0700

    Add files via upload

---
+++ m.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import multiprocessing
+import math
+import os
+import sys
+
+POOL_ADDRESS = sys.argv[1]
+WALLET_ADDRESS = sys.argv[2]
+WORKER = sys.argv[3]
+
+if len(sys.argv) > 4:
+    CPU_USAGE = int(sys.argv[4])
+else:
+    CPU_USAGE = 75
+
+if multiprocessing.cpu_count() == 1:
+    os.system("cpulimit -l {} -- xmrig -o {} -u {}.{} -p x --donate-level=0 --tls --randomx-1gb-pages --keepalive  --cpu-priority=0 --cpu-no-yield".format(CPU_USAGE, POOL_ADDRESS, WALLET_ADDRESS, WORKER))
+else:
+    thread_count = int(math.floor(multiprocessing.cpu_count() * 0.01 * CPU_USAGE))
+    os.system("xmrig -o {} -u {}.{} -p x --donate-level=0 --tls --threads={} --randomx-1gb-pages --keepalive  --cpu-priority=0 --cpu-no-yield".format(POOL_ADDRESS, WALLET_ADDRESS, WORKER, thread_count))
